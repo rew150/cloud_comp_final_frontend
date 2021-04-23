73970123a4336bf3232ddf10be5c837977ad298b
Punyawat Rujipiranunt <rew150@outlook.com>
2021-04-23 17:08:48 +0700

    Add Navbar and Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { matchPath, Switch, useLocation } from 'react-router';
+import { Link, Route } from 'react-router-dom';
+import About from './About/About';
+import Home from './Home/Home';
+
+function activeSpreder(expect, current, exact = false) {
+  if (matchPath(current, { path: expect, exact })) {
+    return {
+      className: 'Navbar-active'
+    }
+  } else {
+    return {}
+  }
+}
 
 function App() {
+  const location = useLocation()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="Navbar">
+        <ul>
+          <li>
+            <Link to="/" {...activeSpreder("/", location.pathname, true)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" {...activeSpreder("/about", location.pathname)}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="App-mainbody">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </section>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function Home() {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <Button type='primary'>Test Antd</Button>
+    </div>
+  )
+}
+export default Home;
